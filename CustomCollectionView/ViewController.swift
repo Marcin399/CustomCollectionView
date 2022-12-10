7568c685a58f656bd48e1ac8bf32890393467cd9
Marcin Janiczek <inspiremarcin@gmail.com>
2022-12-10 12:57:42 +0100

    CustomCollectionLayout

--- CustomCollectionView/ViewController.swift
+++ CustomCollectionView/ViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController, UICollectionViewDataSource {
-   
+    
     private var collectionView = UICollectionView (
         frame: .zero,
-        collectionViewLayout: UICollectionViewFlowLayout()
+        collectionViewLayout: createLayout()
     )
     
     override func viewDidLoad() {
@@ -19,8 +19,46 @@ class ViewController: UIViewController, UICollectionViewDataSource {
         view.addSubview(collectionView)
         collectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
         collectionView.frame = view.bounds
-        collectionView.backgroundColor = .green
+        collectionView.backgroundColor = .white
         collectionView.dataSource = self
+    }
+    
+    static func createLayout() -> UICollectionViewCompositionalLayout {
+
+        let sItem1 = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1)))
+        sItem1.contentInsets.trailing = 4
+        let sItem2 = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1)))
+        sItem2.contentInsets.leading = 4
+
+        let hGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.5))
+        let hGroup = NSCollectionLayoutGroup.horizontal(layoutSize: hGroupSize, subitems: [sItem1, sItem2])
+        hGroup.contentInsets.top = 4
+
+        let mItem = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.5)))
+        mItem.contentInsets.bottom = 4
+
+        let rightGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1))
+        let rightGroup = NSCollectionLayoutGroup.vertical(layoutSize: rightGroupSize, subitems: [mItem, hGroup])
+        rightGroup.contentInsets.leading = 4
+
+        let lItem = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.75)))
+        lItem.contentInsets.top = 4
+        
+        let upItem = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.25)))
+        upItem.contentInsets.bottom = 4
+        
+        let leftGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1))
+        let leftGroup = NSCollectionLayoutGroup.vertical(layoutSize: leftGroupSize, subitems: [upItem, lItem])
+        leftGroup.contentInsets.trailing = 4
+        
+        let mainHGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1))
+        let mainHGroup = NSCollectionLayoutGroup.horizontal(layoutSize: mainHGroupSize, subitems: [leftGroup, rightGroup])
+
+        let section = NSCollectionLayoutSection(group: mainHGroup)
+        section.contentInsets = .init(top: 8, leading: 8, bottom: 8, trailing: 8)
+        section.interGroupSpacing = 8
+       
+        return UICollectionViewCompositionalLayout(section: section)
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -29,14 +67,16 @@ class ViewController: UIViewController, UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 30
+        return 100
     }
 }
 
 
 /*
  Aby stworzyć podstwowy Collection View należy:
- 1.Tworzymy obiekt z UICollectionView. Przekazujemy jako parametr "frame" .zero = CGRect(x: 0, y: 0, width: 0, height: 0)
+ 1.Tworzymy obiekt z UICollectionView. Parametr "frame" .zero = CGRect(x: 0, y: 0, width: 0, height: 0). Parametr collectionViewLayout ...
+ 2.Tworzymy nową klase która dziedziczy po UICollectionViewCell w niej tworzymy obiekt z UIImageView
+ clipsToBounds Ustawienie tej wartości na truepowoduje przycięcie widoków podrzędnych do granic widoku. W przypadku ustawienia na widoki falsepodrzędne, których ramki wykraczają poza widoczne granice widoku, nie są przycinane.
  
  
  
